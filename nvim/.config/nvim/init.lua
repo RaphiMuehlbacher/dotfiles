c23ba272ac077c43468b47abf6e8f7b835f9f711
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-02-09 12:47:59 +0100

    added nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- for transparency
+vim.cmd([[
+  hi CursorLine guibg=none
+]])
---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- -- vim-tmux-navigator settings
+vim.g.tmux_navigator_no_mappings = 1 -- Disable default mappings if you want custom ones
+
+-- Custom keybindings
+vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { silent = true })
+vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { silent = true })
+vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { silent = true })
+vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { silent = true })
+vim.keymap.set('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', { silent = true })
