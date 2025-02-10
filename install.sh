387625d217c868220a1c3ba5cc2e374e60c5d57d
Raphael Mühlbacher <157497042+RaphiMuehlbacher@users.noreply.github.com>
2025-02-10 11:27:15 +0100

    fixed brew installation

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ echo "installing curl"
 sudo apt install -y curl
 
 # brew
+sudo chown -R $(whoami) /home/linuxbrew/.linuxbrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
