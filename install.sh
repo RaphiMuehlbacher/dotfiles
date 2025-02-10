2dc54350f734eb6ce46935bb6d75026b515b069b
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-02-10 11:01:07 +0100

    added bat, zoxide, eza, lazyvim to install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Updating package list"
 sudo apt update -y
 
 echo "Installing essential tools..."
-
+sudo apt install -y build-essential
 # git
 echo "installing git"
 sudo apt install -y git
@@ -15,16 +15,27 @@ git config --global user.email="raphi.muehlbacher@gmail.com"
 echo "installing curl"
 sudo apt install -y curl
 
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 # zsh
 echo "installing zsh"
-sudo apt install zsh
+sudo apt install -y zsh
 chsh -s $(which zsh)
 
 # oh-my-zsh
 echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# starship
+# zsh-autosuggestions
+echo "installing zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+echo "installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh
 
@@ -33,4 +44,26 @@ echo "installing fzf"
 sudo apt install -y fzf
 
 # tmux
-sudo apt install tmux
+echo "installing tmux"
+sudo apt install -y tmux
+
+# fd
+echo "installing fd"
+brew install fd
+
+# eza
+echo "installing eza"
+brew install eza
+
+# zoxide
+echo "installing zoxide"
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# bat
+echo "installing bat"
+brew install bat
+
+# lazyvim
+echo "installing lazyvim"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
